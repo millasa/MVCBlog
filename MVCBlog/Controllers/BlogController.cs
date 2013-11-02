4c376aacb4cfba7ce9a75a071dd6e3ed093dc080
unknown <melena.art@gmail.com>
2013-11-03 00:20:28 +0400

    add categories, articles controllers, views

---
+++ MVCBlog/Controllers/BlogController.cs
@@ -0,0 +1,42 @@
+﻿using MVCBlog.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MVCBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        MVCBlogContext db = new MVCBlogContext();
+
+        //
+        // GET: /Blog/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Blog/BrowseArticlesByCategory/<id>
+
+        public ActionResult BrowseArticlesByCategory(int id)
+        {
+            var categoryArticles = db.Categories.Include("Articles")
+                                .Single(c => c.CategoryId == id);
+
+            return View(categoryArticles);
+        }
+
+        //
+        // GET: /Blog/Article/<id>
+
+        public ActionResult Article(int id)
+        {
+            var article = db.Articles.Include("Comments")
+                        .Single(c => c.ArticleId == id);
+
+            return View(article);
+        }
+
+    }
+}
---
+++ MVCBlog/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using MVCBlog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MVCBlogContext db = new MVCBlogContext();
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var categories = db.Categories.ToList();
+            return View(categories);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
